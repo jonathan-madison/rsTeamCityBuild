59a009df4ea295d9dd4b2f9b47a34dd425a7fa38
Jonathan Madison <jonathan.madison@rackspace.com>
2014-11-20 15:06:19 -0600

    add scratchpad

---
+++ scratchpad.ps1
@@ -0,0 +1,19 @@
+﻿$user = "admin"
+$pass = "fmsource001"
+$statusAPI = ""
+
+$webclient = New-Object System.Net.WebClient
+$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+$webclient.ResponseHeaders
+
+Invoke-RestMethod -uri http://source.doubledutch.me/app/rest/projects -Method Get -Headers @{}
+
+http://teamcity:8111/httpAuth/app/rest/projects
+
+
+$user = "devops"
+$pass = "fmsource001"
+$uri = "http://source.doubledutch.me/app/rest/projects"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$pass)))
+
+$proj = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -uri $uri
